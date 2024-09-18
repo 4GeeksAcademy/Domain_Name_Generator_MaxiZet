0af017aa9c67df7cc1da41ca25e40d8de945a1c3
MaxiZet <98966844+MaxiZet@users.noreply.github.com>
2024-09-18 01:16:35 +0000

    Primera Parte de la entrega

--- src/app.js
+++ src/app.js
@@ -2,10 +2,34 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
+// import { start } from "@popperjs/core";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let starts = ["the", "our"];
+  let mids = ["great", "big"];
+  let ends = ["jogger", "racoon"];
+  let dominios = [".es", ".uy", ".ve", ".br", ".ar"];
+
+  let dominiosNames = [];
+
+  starts.forEach(start => {
+    mids.forEach(mid => {
+      ends.forEach(end => {
+        dominios.forEach(dominio => {
+          let domain = `${start}${mid}${end}${dominio}`;
+          console.log(domain);
+          dominiosNames.push(domain);
+        });
+      });
+    });
+  });
+
+  let listaDeDominios = dominiosNames.map(url => {
+    return '<li class="alert alert-success mt-2">' + url + "</li>";
+  });
+
+  let urlList = document.getElementById("url-list");
+  urlList.innerHTML = listaDeDominios.join("");
 };
